71d1c5e981865daf79c55f9a3df50d47f6b81d82
ftnext <takuyafjp+develop@gmail.com>
2022-10-26 01:26:19 +0900

    [feat] Enable to build new tab link HTML

---
+++ sphinx_new_tab_link/__init__.py
@@ -0,0 +1,25 @@
+from sphinx.writers.html import HTMLTranslator
+
+
+class NewTabLinkHTMLTranslator(HTMLTranslator):
+    """Patched translator to open an external link in a new tab of the browser.
+
+    ref: https://stackoverflow.com/a/67153583
+    """
+
+    def starttag(self, node, tagname, *args, **atts):
+        if (
+            tagname == "a"
+            and "target" not in atts
+            and (
+                "external" in atts.get("class", "")
+                or "external" in atts.get("classes", [])
+            )
+        ):
+            atts["target"] = "_blank"
+            atts["rel"] = "noopener noreferrer"
+        return super().starttag(node, tagname, *args, **atts)
+
+
+def setup(app):
+    app.set_translator("html", NewTabLinkHTMLTranslator)
