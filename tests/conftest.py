d4ee262f382a3173c50d2c148e3114a4a7914f2c
ftnext <takuyafjp+develop@gmail.com>
2022-10-26 01:22:17 +0900

    [test] Configure pytest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from sphinx.testing.path import path
+
+pytest_plugins = "sphinx.testing.fixtures"
+collect_ignore = ["roots"]
+
+
+@pytest.fixture(scope="session")
+def rootdir():
+    return path(__file__).parent.abspath() / "roots"
