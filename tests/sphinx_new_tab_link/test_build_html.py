2f7515d2c803d737442ad3a8918ce69ab0c7b533
ftnext <takuyafjp+develop@gmail.com>
2022-10-26 01:24:39 +0900

    [test] Add first Sphinx E2E

---
+++ tests/sphinx_new_tab_link/test_build_html.py
@@ -0,0 +1,21 @@
+from io import StringIO
+
+import pytest
+from bs4 import BeautifulSoup
+from sphinx.testing.util import SphinxTestApp
+
+
+@pytest.mark.sphinx("html", testroot="default")
+def test_markup_external_link_should_open_new_tab(
+    app: SphinxTestApp, status: StringIO, warning: StringIO
+):
+    app.build()
+    html = (app.outdir / "index.html").read_text()
+    soup = BeautifulSoup(html, "html.parser")
+    references = soup.find_all("a", {"class": "reference"})
+
+    ref = references[0]
+    assert ref["href"] == "https://example.com/"
+    assert "external" in ref["class"]
+    assert ref["target"] == "_blank"
+    assert ref["rel"] == ["noopener", "noreferrer"]
