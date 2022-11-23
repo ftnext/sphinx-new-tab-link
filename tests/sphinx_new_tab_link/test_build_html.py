455707742fb9ef620b483f7c2eccc2a92280f5d4
ftnext <takuyafjp+develop@gmail.com>
2022-11-23 16:03:32 +0900

    [refactor] Parameterize to add more build_html cases

--- tests/sphinx_new_tab_link/test_build_html.py
+++ tests/sphinx_new_tab_link/test_build_html.py
@@ -5,33 +5,26 @@ from bs4 import BeautifulSoup
 from sphinx.testing.util import SphinxTestApp
 
 
+@pytest.mark.parametrize(
+    "index,expected_url",
+    [(0, "https://example.com/"), (1, "http://abehiroshi.la.coocan.jp")],
+    ids=["markup_external_link", "raw_external_link"],
+)
 @pytest.mark.sphinx("html", testroot="default")
-def test_markup_external_link_should_open_new_tab(
-    app: SphinxTestApp, status: StringIO, warning: StringIO
+def test_should_open_new_tab(
+    app: SphinxTestApp,
+    status: StringIO,
+    warning: StringIO,
+    index: int,
+    expected_url: str,
 ):
     app.build()
     html = (app.outdir / "index.html").read_text()
     soup = BeautifulSoup(html, "html.parser")
     references = soup.find_all("a", {"class": "reference"})
 
-    ref = references[0]
-    assert ref["href"] == "https://example.com/"
-    assert "external" in ref["class"]
-    assert ref["target"] == "_blank"
-    assert ref["rel"] == ["noopener", "noreferrer"]
-
-
-@pytest.mark.sphinx("html", testroot="default")
-def test_raw_external_link_should_open_new_tab(
-    app: SphinxTestApp, status: StringIO, warning: StringIO
-):
-    app.build()
-    html = (app.outdir / "index.html").read_text()
-    soup = BeautifulSoup(html, "html.parser")
-    references = soup.find_all("a", {"class": "reference"})
-
-    ref = references[1]
-    assert ref["href"] == "http://abehiroshi.la.coocan.jp"
+    ref = references[index]
+    assert ref["href"] == expected_url
     assert "external" in ref["class"]
     assert ref["target"] == "_blank"
     assert ref["rel"] == ["noopener", "noreferrer"]
