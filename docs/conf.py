f2ae70d92c2d9ad390381da4c8677687ec634ec3
ftnext <takuyafjp+develop@gmail.com>
2022-11-27 17:10:26 +0900

    [style] Format by black

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,9 @@ sys.path.insert(0, os.path.abspath("../sphinx_new_tab_link"))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sphinx-new-tab-link documentation'
-copyright = '2022, nikkie'
-author = 'nikkie'
+project = "sphinx-new-tab-link documentation"
+copyright = "2022, nikkie"
+author = "nikkie"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,13 +24,13 @@ extensions = [
     "sphinx.ext.githubpages",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'ja'
+language = "ja"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
