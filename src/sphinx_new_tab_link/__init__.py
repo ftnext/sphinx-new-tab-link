5dbe50172f2f5eca384ba85d00649557ca57f5ac
ftnext <takuyafjp+develop@gmail.com>
2024-06-29 00:09:24 +0900

    [refactor] Extract icon svg

--- src/sphinx_new_tab_link/__init__.py
+++ src/sphinx_new_tab_link/__init__.py
@@ -4,6 +4,8 @@ from docutils.nodes import Text, raw
 from sphinx.application import Sphinx
 from sphinxcontrib.kasane import new_translator_class_for_builder
 
+from sphinx_new_tab_link.extras import external_link_icon_html
+
 __VERSION__ = "0.4.0"
 
 
@@ -26,13 +28,7 @@ class NewTabLinkHTMLTranslatorMixin:
             atts["rel"] = "noopener noreferrer"
             if self.builder.config.new_tab_link_show_external_link_icon:
                 node[0] = Text(node[0].astext() + " ")
-                node.append(
-                    raw(
-                        # https://primer.style/foundations/icons/link-external-16/
-                        text='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M3.75 2h3.5a.75.75 0 0 1 0 1.5h-3.5a.25.25 0 0 0-.25.25v8.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25v-3.5a.75.75 0 0 1 1.5 0v3.5A1.75 1.75 0 0 1 12.25 14h-8.5A1.75 1.75 0 0 1 2 12.25v-8.5C2 2.784 2.784 2 3.75 2Zm6.854-1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1Z"></path></svg>',
-                        format="html",
-                    )
-                )
+                node.append(raw(text=external_link_icon_html(), format="html"))
         return super().starttag(node, tagname, *args, **atts)
 
 
