9110968dfce05a4d69a7f794a77d135a7bf33c16
ftnext <takuyafjp+develop@gmail.com>
2024-08-05 23:13:37 +0900

    [refactor] Pass directory name with pytest's fixture

--- tests/test_show_external_link_icon.py
+++ tests/test_show_external_link_icon.py
@@ -7,13 +7,19 @@ from bs4 import BeautifulSoup
 
 @pytest.fixture
 def parsed_built_html(
+    request,
     make_app,
     sphinx_test_tempdir: Path,
     rootdir: Path,
 ):
-    srcdir = sphinx_test_tempdir / "external-link-icon"
+    marker = request.node.get_closest_marker("sphinx_build_in_tempdir")
+    if marker is None:
+        raise RuntimeError("Mark as `sphinx_build_in_tempdir`")
+    directory_name = marker.args[0]
+
+    srcdir = sphinx_test_tempdir / directory_name
     if not srcdir.exists():
-        testroot_path = rootdir / "test-external-link-icon"
+        testroot_path = rootdir / f"test-{directory_name}"
         shutil.copytree(testroot_path, srcdir)
 
     app = make_app("html", srcdir=srcdir)
@@ -29,6 +35,7 @@ def assert_is_external(reference, expected_url: str) -> None:
     assert reference["rel"] == ["noopener", "noreferrer"]
 
 
+@pytest.mark.sphinx_build_in_tempdir("external-link-icon")
 def test_see_external_link_icon(parsed_built_html):
     references = parsed_built_html.find_all("a", {"class": "reference"})
 
@@ -38,6 +45,7 @@ def test_see_external_link_icon(parsed_built_html):
     assert_is_external(ref, "https://pypi.org/project/sphinx-new-tab-link/")
 
 
+@pytest.mark.sphinx_build_in_tempdir("external-link-icon")
 def test_can_see_icon_with_image_directive_target(parsed_built_html):
     # https://github.com/ftnext/sphinx-new-tab-link/issues/16
     references = parsed_built_html.find_all("a", {"class": "reference"})
@@ -52,6 +60,7 @@ def test_can_see_icon_with_image_directive_target(parsed_built_html):
     assert ref.svg
 
 
+@pytest.mark.sphinx_build_in_tempdir("external-link-icon")
 def test_can_see_icon_with_figure_directive_target(parsed_built_html):
     references = parsed_built_html.find_all("a", {"class": "reference"})
 
