d0f0c84c10a0c952cc573af2ab9cf81f796b7290
ftnext <takuyafjp+develop@gmail.com>
2024-08-08 16:11:26 +0900

    [refactor] Try small fixtures in test module

--- tests/test_custom_translator.py
+++ tests/test_custom_translator.py
@@ -5,19 +5,38 @@ import pytest
 from bs4 import BeautifulSoup
 
 
-@pytest.mark.parametrize("testroot", ["method-assignment", "dynamic-class"])
-def test_should_open_new_tab(
-    make_app, sphinx_test_tempdir: Path, rootdir: Path, testroot: str
-):
-    srcdir = sphinx_test_tempdir / testroot
+@pytest.fixture
+def builder() -> str:
+    return "html"
+
+
+@pytest.fixture(params=["method-assignment", "dynamic-class"])
+def directory_name(request) -> str:
+    return request.param
+
+
+@pytest.fixture
+def prepared_srcdir(
+    sphinx_test_tempdir: Path, rootdir: Path, directory_name: str
+) -> Path:
+    srcdir = sphinx_test_tempdir / directory_name
     if not srcdir.exists():
-        testroot_path = rootdir / f"test-{testroot}"
+        testroot_path = rootdir / f"test-{directory_name}"
         shutil.copytree(testroot_path, srcdir)
 
-    app = make_app("html", srcdir=srcdir)
+    return srcdir
+
+
+@pytest.fixture
+def built_html_path(make_app, builder: str, prepared_srcdir: Path) -> Path:
+    app = make_app(builder, srcdir=prepared_srcdir)
     app.build()
 
-    html = (app.outdir / "index.html").read_text()
+    return app.outdir / "index.html"
+
+
+def test_should_open_new_tab(built_html_path: Path) -> None:
+    html = built_html_path.read_text()
     soup = BeautifulSoup(html, "html.parser")
     references = soup.find_all("a", {"class": "reference"})
 
