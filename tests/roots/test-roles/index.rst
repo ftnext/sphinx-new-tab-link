8b1e49bc3da86714ecc9788edbd91c5f71716ca2
ftnext <takuyafjp+develop@gmail.com>
2024-09-01 19:50:17 +0900

    [test] Test icon-link role

---
+++ tests/roots/test-roles/index.rst
@@ -0,0 +1,7 @@
+=========
+Role test
+=========
+
+External link (without icon): `Example <https://example.com/>`__
+
+External link with icon: :icon-link:`httpbin <https://httpbin.org/>`
---
+++ tests/test_roles.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from .helpers import assert_reference_is_external_with_icon, extract_references
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+@pytest.fixture
+def builder() -> str:
+    return "html"
+
+
+@pytest.fixture
+def directory_name() -> str:
+    return "roles"
+
+
+def test_see_external_link_with_icon(built_html_path: Path) -> None:
+    references = extract_references(built_html_path)
+
+    ref = references[1]
+    assert_reference_is_external_with_icon(ref, "https://httpbin.org/")
+    assert ref.text == "httpbin "
