4f2377805880771ef645609fd1934a8b9c7628e9
ftnext <takuyafjp+develop@gmail.com>
2024-09-04 23:39:01 +0900

    [docs] Explain IconLinkRole

--- src/sphinx_new_tab_link/roles.py
+++ src/sphinx_new_tab_link/roles.py
@@ -7,6 +7,15 @@ from sphinx_new_tab_link.core import add_icon_to_reference
 
 
 class IconLinkRole(ReferenceRole):
+    """Role to create a external link with an icon.
+
+    Of course, opened in new tabs of your browser.
+
+    Usage::
+
+        :icon-link:`title <target>`
+    """
+
     def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
         node = nodes.reference(text=self.title, refuri=self.target)
         reference_with_icon = add_icon_to_reference(node)
